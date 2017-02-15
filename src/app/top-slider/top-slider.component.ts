c9c1b106354f34dfc4272bfddb16ded24006ff86
David Wolff <info@davidwolff.dk>
2017-02-15 13:41:04 +0100

    created top slider and added some initial styles

--- src/app/top-slider/top-slider.component.ts
+++ src/app/top-slider/top-slider.component.ts
@@ -5,11 +5,53 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './top-slider.component.html',
   styleUrls: ['./top-slider.component.scss']
 })
-export class TopSliderComponent implements OnInit {
+export class TopSliderComponent {
 
-  constructor() { }
+  constructor() {  }
+
+  public intervalNumber: number = 300000;
+  public slides: any[] = [];
+  public activeSlideIndex: number;
+  public noWrapSlides: boolean = false;
 
   ngOnInit() {
+    this.addSlide();
   }
+
+  addSlide() {
+    this.slides.push(
+      {
+        image: "assets/images/slider1_opt.jpg",
+        heading: "slide 1",
+        text: "asasd asd  asdfkjhsdf sdfhjksf sdfkljs sdf sdf sf",
+        link: "http://www.google.com"
+      },
+      {
+        image: "assets/images/slider2_opt.jpg",
+        heading: "slide 2",
+        text: "asasd asd  asdfkjhsdf sdfhjksf sdfkljs sdf sdf sf",
+        link: "http://www.google.com"
+      },
+      {
+        image: "assets/images/slider3_opt.jpg",
+        heading: "slide 3",
+        text: "asasd asd  asdfkjhsdf sdfhjksf sdfkljs sdf sdf sf",
+        link: "http://www.google.com"
+      },
+      {
+        image: "assets/images/slider4_opt.jpg",
+        heading: "slide 4",
+        text: "asasd asd  asdfkjhsdf sdfhjksf sdfkljs sdf sdf sf",
+        link: "http://www.google.com"
+      },
+      {
+        image: "assets/images/slider5_opt.jpg",
+        heading: "slide 5",
+        text: "asasd asd  asdfkjhsdf sdfhjksf sdfkljs sdf sdf sf",
+        link: "http://www.google.com"
+      }
+    )
+  }
+
 
 }
